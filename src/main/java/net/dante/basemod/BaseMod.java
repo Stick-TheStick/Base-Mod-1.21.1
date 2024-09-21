794ca141c833475e63496b8a31f0c08c7c7c62d9
Stick <samuelmahendiran@gmail.com>
2024-09-21 12:11:54 -0500

    Added example item

--- src/main/java/net/dante/basemod/BaseMod.java
+++ src/main/java/net/dante/basemod/BaseMod.java
@@ -1,5 +1,6 @@
 package net.dante.basemod;
 
+import net.dante.basemod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class BaseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/dante/basemod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.dante.basemod.item;
+
+import net.dante.basemod.BaseMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item EXAMPLE_ITEM = registerItem("example_item", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(BaseMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        BaseMod.LOGGER.info("Registering Mod Items for" + BaseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(EXAMPLE_ITEM);
+        });
+    }
+}
