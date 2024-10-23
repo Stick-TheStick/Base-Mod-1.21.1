41397738f7ed8b97e0aa26bd3eb02567647d640c
KaputGosling902 <89622124+KaputGosling902@users.noreply.github.com>
2024-10-23 18:07:20 -0500

    Updated Mod Tags

--- src/main/java/net/dante/basemod/datagen/ModItemTagProvider.java
+++ src/main/java/net/dante/basemod/datagen/ModItemTagProvider.java
@@ -1,4 +1,21 @@
 package net.dante.basemod.datagen;
 
-public class ModItemTagProvider {
+import net.dante.basemod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture, @Nullable BlockTagProvider blockTagProvider) {
+        super(output, completableFuture, blockTagProvider);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.Transformable_Item)
+                .add(ModItems.EXAMPLE_ITEM);
+    }
 }
