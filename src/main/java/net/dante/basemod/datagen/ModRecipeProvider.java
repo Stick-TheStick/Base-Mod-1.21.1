d74efa9bf3b451fcc4b4fceaae9f7e1d7d0135a4
KaputGosling902 <89622124+KaputGosling902@users.noreply.github.com>
2024-10-23 19:26:07 -0500

    Finished Datagen implementation

--- src/main/java/net/dante/basemod/datagen/ModRecipeProvider.java
+++ src/main/java/net/dante/basemod/datagen/ModRecipeProvider.java
@@ -1,4 +1,28 @@
 package net.dante.basemod.datagen;
 
-public class ModRecipeProvider {
+import net.dante.basemod.block.ModBlocks;
+import net.dante.basemod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        List<ItemConvertible> EXAMPLE_ITEM_SMELTABLES = List.of(ModItems.EXAMPLE_ITEM, ModBlocks.EXAMPLE_BLOCK,
+                ModBlocks.EXAMPLE_BLOCK);
+
+        offerSmelting(exporter, EXAMPLE_ITEM_SMELTABLES, RecipeCategory.MISC, ModItems.EXAMPLE_ITEM, .25f, 200, "example_item");
+
+    }
 }
