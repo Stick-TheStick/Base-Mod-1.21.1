2568958d18f1b373004acc9d51bf53bbefa4e57b
KaputGosling902 <89622124+KaputGosling902@users.noreply.github.com>
2024-11-20 22:40:06 -0600

    fixed block transparency

--- src/main/java/net/dante/basemod/BaseModClient.java
+++ src/main/java/net/dante/basemod/BaseModClient.java
@@ -1,10 +1,13 @@
 package net.dante.basemod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.dante.basemod.block.ModBlocks; // Adjust this import as needed
 
 public class BaseModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WITHERED_GRASS, RenderLayer.getCutout());
     }
 }
